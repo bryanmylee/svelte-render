abc0708db0146b22fd01cc419eb682ac5ba5f270
Bryan Lee <bryanmylee@gmail.com>
2022-09-28 19:16:52 +1000

    refactor: simplify createRender definition

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -1,14 +1,7 @@
 import type {SvelteComponent} from 'svelte';
 import type {Readable} from 'svelte/store';
-import type {ComponentRenderConfig, EmptyProps, SvelteComponentWithProps} from './types';
+import type {SvelteComponentWithProps} from './types';
 
-export function createRender<TProps extends EmptyProps, TComponent extends SvelteComponent>(
-	component: SvelteComponentWithProps<TProps, TComponent>,
-): ComponentRenderConfig<TProps, TComponent>;
-export function createRender<TProps extends object, TComponent extends SvelteComponent>(
-	component: SvelteComponentWithProps<TProps, TComponent>,
-	props: TProps | Readable<TProps>,
-): ComponentRenderConfig<TProps, TComponent>;
 export function createRender<TProps extends object, TComponent extends SvelteComponent>(
 	component: SvelteComponentWithProps<TProps, TComponent>,
 	props?: TProps | Readable<TProps>,
