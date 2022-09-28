8dc37fdc5be302c4fe81b1af40f0adbe197948f6
Bryan Lee <bryanmylee@gmail.com>
2022-09-28 20:43:12 +1000

    test: event handling

---
+++ src/tests/event-handling.test.ts
@@ -0,0 +1,21 @@
+import {createRender} from '$lib';
+import {render, screen} from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import InteractiveRocket from './interactive-rocket.svelte';
+import Template from './template.svelte';
+
+it('receives forwarded events', async () => {
+	const click = vi.fn();
+	const launch = vi.fn();
+	const config = createRender(InteractiveRocket).on('click', click).on('launch', launch);
+	render(Template, {props: {config}});
+
+	const nativeButton = screen.getByText('👽');
+	const customButton = screen.getByText('🚀');
+
+	const user = userEvent.setup();
+	await user.click(nativeButton);
+	expect(click).toHaveBeenCalled();
+	await user.click(customButton);
+	expect(click).toHaveBeenCalled();
+});
