edc92a09bfef908c836620cdbcb7946dc52ecdda
Bryan Lee <bryanmylee@gmail.com>
2022-09-28 19:57:18 +1000

    fix: allow omission of prop arg

--- src/lib/createRender.ts
+++ src/lib/createRender.ts
@@ -1,6 +1,16 @@
-import type {ComponentProps, SvelteComponent} from 'svelte';
+import type {ComponentProps, SvelteComponent, SvelteComponentTyped} from 'svelte';
 import type {Readable} from 'svelte/store';
 import type {Constructor, RenderConfig} from './types';
+
+// Allow omission of the `props` argument if the component accepts no props.
+export function createRender<TComponent extends SvelteComponentTyped<Record<string, never>>>(
+	component: Constructor<TComponent>,
+): RenderConfig<TComponent>;
+
+export function createRender<TComponent extends SvelteComponent>(
+	component: Constructor<TComponent>,
+	props: ComponentProps<TComponent> | Readable<ComponentProps<TComponent>>,
+): RenderConfig<TComponent>;
 
 export function createRender<TComponent extends SvelteComponent>(
 	component: Constructor<TComponent>,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,26 @@
+<script lang="ts">
+	import {createRender, Render} from '$lib';
+	import Rocket from '../tests/rocket.svelte';
+	import MultipleRockets from '../tests/multiple-rockets.svelte';
+	import InteractiveRocket from '../tests/interactive-rocket.svelte';
+
+	const rocket = createRender(Rocket);
+	const multipleRockets = createRender(MultipleRockets, {times: 3});
+	const interactiveRocket = createRender(InteractiveRocket);
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+	<Render of={rocket} />
+</div>
+
+<div>
+	<Render of={multipleRockets} />
+</div>
+
+<div>
+	<Render of={interactiveRocket} />
+</div>
